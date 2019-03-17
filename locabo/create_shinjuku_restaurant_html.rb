b83eb7695c4da7c66f0ea2a6189b6efaa12058d5
longtime1116 <longtime1116@gmail.com>
2019-03-17 17:13:27 +0900

    output html

--- locabo/create_shinjuku_restaurant_html.rb
+++ locabo/create_shinjuku_restaurant_html.rb
@@ -1,10 +1,19 @@
 require "csv"
 require "pp"
 
+def build_comments(comments_str)
+  li = comments_str.map do |comment_str|
+    "<li>#{comment_str}</li>"
+  end.join
+
+  "<ul>#{li}</ul>"
+end
+
 outfile = File.open("./locabo_shinjuku_restaurant.html", "w")
 outfile.puts(File.read("./txt/shinjuku_header.txt"))
 
-station_restaurants_hash ={}
+# csv parse
+station_restaurants_hash = {}
 CSV.foreach("./locabo.csv", headers: true) do |f|
   next if f["最寄り駅"].nil?
   unless station_restaurants_hash.keys.include?(f["最寄り駅"])
@@ -23,9 +32,18 @@ CSV.foreach("./locabo.csv", headers: true) do |f|
   }
 end
 
+# output html from parsed data
 station_restaurants_hash.each do |station, restaurants|
-  p station
+  next unless station.include?("新宿")
+  h2_station = "&nbsp;<h2>#{station}駅</h2>"
+  outfile.puts(h2_station)
   restaurants.each do |restaurant|
-    pp restaurant
+    need_delete = !restaurant[:open]
+    title = "<a href=\"#{restaurant[:url]}\">#{restaurant[:name]}</a>(#{restaurant[:genre]})"
+    title = "<del>#{title}</del>" if need_delete
+    comments = build_comments(["#{station}駅より#{restaurant[:walk]}", restaurant[:comments]].flatten)
+
+    outfile.puts(title)
+    outfile.puts(comments)
   end
 end
