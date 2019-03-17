6a3cf5a49a2be8f240aeaffeebb33c740807ade3
longtime1116 <longtime1116@gmail.com>
2019-03-17 16:12:05 +0900

    change directory of input text file

--- locabo/create_tokyo_restaurant_html.rb
+++ locabo/create_tokyo_restaurant_html.rb
@@ -9,7 +9,7 @@ def build_comments(comments_str)
 end
 
 outfile = File.open("./locabo_tokyo_restaurant.html", "w")
-outfile.puts(File.read("./header.txt"))
+outfile.puts(File.read("./txt/tokyo_header.txt"))
 
 regions = []
 # 想定する headers は ["営業状況", "店名", "ジャンル", "最寄り駅", "地域", "コメント", "URL"]
